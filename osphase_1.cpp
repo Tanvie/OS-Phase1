8b520b32f5fb4c0b8e08d87892f8e044ea5b6756
Tanvi Wakade <64800176+Tanvie@users.noreply.github.com>
2020-12-04 22:25:41 +0530

    CR LR SR added

--- osphase_1.cpp
+++ osphase_1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 ifstream fin;
 ofstream fout;
-void INIT(char M[100][4], char *IR, char *R, int *C, int *IC)
+void INIT(char M[100][4], char *IR, char *R, int *C, int *IC,int *SI)
 {
     for (int i = 0; i < 100; i++)
         for (int j = 0; j < 4; j++)
@@ -15,12 +15,66 @@ void INIT(char M[100][4], char *IR, char *R, int *C, int *IC)
         R[i] = '\0';
     }
     IC[0] = 0;
+    SI[0] =0;
     //C[0] = 0;
 }
-void EXECUTEUSERPROGRAM(char M[100][4], char *IR, char *R, int *C, int IC)
+void getData(char M[100][4],char *IR)
+{
+    int a1 = IR[1] - '0';
+    int a0 = IR[0] - '0';
+    int mem = a1 * 10 + a0;
+    
+
+}
+void putData(char M[100][4],char *IR)
+{
+}
+void loadData(char M[100][4], char *IR, char *R)
+{
+    int a1 = IR[1] - '0';
+    int a0 = IR[0] - '0';
+    int mem = a1 * 10 + a0;
+    R[0] = M[mem][0];
+    R[1] = M[mem][1];
+    R[2] = M[mem][2];
+    R[3] = M[mem][3];
+}
+void storeData(char M[100][4], char *IR, char *R)
+{
+    int a1 = IR[1] - '0';
+    int a0 = IR[0] - '0';
+    int mem = a1 * 10 + a0;
+    M[mem][0] = R[0];
+    M[mem][1] = R[1];
+    M[mem][2] = R[2];
+    M[mem][3] = R[3];
+}
+void compare(char M[100][4], char *IR, char *R, int *C)
+{
+    int a1 = IR[1] - '0';
+    int a0 = IR[0] - '0';
+    int mem = a1 * 10 + a0;
+    char *operand2;
+    int result;
+    operand2[0] = M[mem][0];
+    operand2[1] = M[mem][1];
+    operand2[2] = M[mem][2];
+    operand2[3] = M[mem][3];
+
+    result = (operand2[0] == R[0]);
+    for (int i = 1; i < 4; i++)
+    {
+        result &= (operand2[i] == R[i]);
+    }
+
+    C[0] = result;
+}
+
+void EXECUTEUSERPROGRAM(char M[100][4], char *IR, char *R, int *C, int IC,int *SI)
 {
     IC = 0;
-    while(M[IC][0]!='H')
+    SI[0] = 0;
+    while (M[IC][0] != 'H')
     {
         //strcpy(M[IC],IR);
         IR[0] = M[IC][0];
@@ -30,16 +84,38 @@ void EXECUTEUSERPROGRAM(char M[100][4], char *IR, char *R, int *C, int IC)
         IC++;
         // cout<<__LINE__<<"\t";
         // cout<<IR[0]<<IR[1]<<IR[2]<<IR[3]<<endl;
-        
-        
+        switch (IR[0])
+        {
+        case 'G':
+            SI[0] = 1;
+            break;
+        case 'P':
+            SI[0] = 2;
+            break;
+        case 'L':
+            loadData(M, IR, R);
+            break;
+        case 'S':
+            storeData(M, IR, R);
+            break;
+        case 'C':
+            compare(M, IR, R, C);
+            break;
+        case 'B':
+            //branch if true
+            break;
+        default:
+            cout << "\nINVALID INSTRUCTION!!!";
+            exit(1);
+        }
     }
 }
-void STARTEXECUTION(char M[100][4], char *IR, char *R, int *C, int *IC)
+void STARTEXECUTION(char M[100][4], char *IR, char *R, int *C, int *IC,int *SI)
 {
     IC[0] = 0;
-    EXECUTEUSERPROGRAM(M, IR, R, C, IC[0]);
+    EXECUTEUSERPROGRAM(M, IR, R, C, IC[0],SI);
 }
-void LOAD(char M[100][4], char *IR, char *R, int *C, int *IC)
+void LOAD(char M[100][4], char *IR, char *R, int *C, int *IC,int *SI)
 {
     int instC, pcCount;
     int m = 0;
@@ -67,19 +143,20 @@ void LOAD(char M[100][4], char *IR, char *R, int *C, int *IC)
 
         else if (!(line.find("$DTA") == string::npos))
         {
-            STARTEXECUTION(M, IR, R, C, IC);
+            STARTEXECUTION(M, IR, R, C, IC,SI);
+
         }
-        else
+        else if (!(line.find("$END") == string::npos))
             cout << "";
+        //mem exceed condition
     }
-    for (int i = 0; i < 5; i++)
-    {
-        cout << "FOR DEBUG IN LOAD" << endl;
-        cout << endl
-             << endl;
-        cout << M[i][0] << M[i][1] << M[i][2] << M[i][3] << endl;
-        
-    }
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cout << "FOR DEBUG IN LOAD" << endl;
+    //     cout << endl
+    //          << endl;
+    //     cout << M[i][0] << M[i][1] << M[i][2] << M[i][3] << endl;
+    // }
 }
 int main()
 {
@@ -90,10 +167,11 @@ int main()
     char R[4];
     int *C;
     int *IC;
+    int *SI;
     // while(1)
     {
-        INIT(M, IR, R, C, IC);
-        LOAD(M, IR, R, C, IC);
+        INIT(M, IR, R, C, IC,SI);
+        LOAD(M, IR, R, C, IC,SI);
     }
 
     fin.close();
